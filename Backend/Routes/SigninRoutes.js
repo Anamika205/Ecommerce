9ae6da302d756470c41329352a1cb2d667720a69
anamika <anamikahajipur2000@gmail.com>
2024-10-25 23:33:11 +0530

    update code

--- Backend/Routes/SigninRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const routes = express.Router();
-const signapi = require('../Controller/signinController');
-
-routes.post('/addnewadmin', signapi.AddnewAdmin);
-
-module.exports = routes;
--- Backend/Routes/loginRoutes.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const routes = express.Router();
-const loginapi = require('../Controller/loginController');
-
-routes.post('/addlogin', loginapi.createLogin);
-
-module.exports = routes;
