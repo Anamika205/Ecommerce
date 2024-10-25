f7aa454dd8f3f014f92b5ea0d7d549b67af3220a
anamika <anamikahajipur2000@gmail.com>
2024-10-25 23:16:29 +0530

    update code

--- src/Assets/Data.js
+++ src/Assets/Data.js
@@ -6,11 +6,5 @@ const DataItem = [
     { id: 5, src: '/Image/image5.png', alt: 'image5' }]
 
 
- const WomenItem = [
-    {id:1, src:'/WomenImg/image1.png', alt:"image1"},
-    {id:2, src:'/WomenImg/image2.png', alt:"image2"},
-    {id:3, src:'/WomenImg/image3.png', alt:"image3"},
-    {id:4, src:'/WomenImg/image4.png', alt:"image4"}]
-
 
 export default DataItem
